aa3f2b1ab60956b7168f02a089666060c54eab13
Raghunath Rao Arcot <106887145+Raghunath-arcot@users.noreply.github.com>
2023-10-01 20:24:30 +0530

    Add files via upload

---
+++ basic-android-kotlin-compose-birthday-card-app-main (1)/basic-android-kotlin-compose-birthday-card-app-main/app/src/main/java/com/example/happybirthday/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.happybirthday.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val light_primary = Color(0xFFD0BCFF)
+val light_secondary = Color(0xFFCCC2DC)
+val light_tertiary = Color(0xFFEFB8C8)
+
+val dark_primary = Color(0xFF6650a4)
+val dark_secondary = Color(0xFF625b71)
+val dark_tertiary = Color(0xFF7D5260)
